823226de13be1db0458687037e3500c80471ab4d
Little Milo <no-reply>
2019-07-13 07:42:02 -0700

    if else logic not failing now

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ state = {
   };
 
   componentDidMount() {
+    console.log('======= called did mount ======= ');
+    
     // console.log(this.state.data)
     // Everytime the component mounts we want to update the state.data object
     // so that state.data is this.state.data shuffled. So it should call your shuffle function
@@ -26,30 +28,41 @@ state = {
   }
             // passing array, this.state.data
   resetGame = (data) => {
+    console.log("reset game")
   // This function should do the following:
+  console.log(this.state.data)
+  console.log(data)
   
   // map through data and for each item in the array reset the clicked to false. Make sure to store the
   // full results of the map to a temp variable.
-  // let temp= data.map(eachItem => (
-  //   <Card 
-  //   id={eachItem.id}
-  //   key={eachItem.id}
-  //   image={eachItem.image}
-    // isClicked={eachItem.false}
-    // />
-    // ));
+  let temp= this.state.data.map(eachItem => (
+    {...eachItem, isClicked: false}
+    ));
+
+  console.log('------------ Temp ---------------')
+
+    
+    console.log(temp);
     
     // reset the state.score to 0.
     // update the state and set data equal to the temp variable shuffled.
-    // this.setState({
-    //   data: temp,
-    //   score: 0
-    // })
+    this.setState({
+      data: temp,
+      score: 0
+    })
+
+  console.log('------------ after setState ---------------')
+
+  console.log(this.state.data)
+
+  
   };
 
   //
-  shuffle = (data) => {
-    let temp= data.map(eachItem => (
+  shuffle = (mydata) => {
+    console.log('$$$$$$$$$ shuffle ccalled $$$$$$$$$$$');
+    
+    let temp= mydata.map(eachItem => (
       <Card 
       id={eachItem.id}
       key={eachItem.id}
@@ -71,6 +84,7 @@ state = {
 
   handleItemClick = (event) => {
     // you'll want to set up a utility variable called isFirstClick and set it to false
+    let firstClick = false
     // You'll create a copy of your state data by mapping through this.state.data
     // for each item create a copy of the object then check to see if that objects id is equal to the
     // id that was passed into the handleClick function. If the ids match, you should check to see if the
@@ -79,27 +93,59 @@ state = {
    let id = event.target.id
   //  console.log(id)
   //  console.log(this.state.data[id])
-  console.log(typeof id) // returns string
+  // console.log(typeof id) // returns string
   let upDateData = this.state.data.map(element => {
-    console.log(typeof element.id) // retursn number
+    //console.log(typeof element.id) // retursn number
 
-    if (element.id === parseInt(id) && element.isClicked !== true) {
-      console.log(element.name)
-      
-      return {...element, isClicked: true}
-      // Using spread operator same as return below. 
-      // return {
-      //   "id": element.id,
-      //   "name": element.name,
-      //   "image": element.image,
-      //   "isClicked": true
-      // }
-    }
-    return element
-  })
+        if (element.id === parseInt(id)) {
+
+          if(element.isClicked === false){
+            // User got a point
+            firstClick = true
+          }
+          
+          if(firstClick){
+            // update score
+            let newScore= this.state.score + 1
+            let newTopScore= Math.max(newScore, this.state.topScore)
+            this.setState({score: newScore, topScore: newTopScore })
+            
+
+            return {...element, isClicked: true}
+          }
+          else{
+            //gameover
+            this.resetGame()
+          }
+
+        }
+
+        return {...element}
+
+
+    // if (element.id === parseInt(id) && element.isClicked === false) {
+    //   console.log(element.name)
+    //   // increment score & topscore
+    //   // Math.max
+    //   return {...element, isClicked: true}
+    //   // Using spread operator same as return below. 
+    //   // return {
+    //   //   "id": element.id,
+    //   //   "name": element.name,
+    //   //   "image": element.image,
+    //   //   "isClicked": true
+    //   // }
+    // } // end of if
+    // else if (element.id === parseInt(id) && element.isClicked === true) {
+    //   // gameover
+    //   this.resetGame(this.state.data)
+    // } else {
+    //   return {...element}
+    // }
+ }) // end of map
+
   this.setState({data: upDateData})
-
-  console.log(this.state.data)
+  // console.log(this.state.data)
     // Now we If the image has never been clicked, so
     // * If isFirstClick === true then
     // you want to increment the score and you want to set top score equal to whichever is greater score or current top score
@@ -107,6 +153,7 @@ state = {
 
     // * If isFirstClick === false then
     // we wantmto call the resetGame function
+  
 
   };
   // Only method needed to be defined is React.Component subclass render()
