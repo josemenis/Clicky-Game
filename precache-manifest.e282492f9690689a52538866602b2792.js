0f24db6335f482f20d4aae4811f93d28342fa5fd
Little Milo <no-reply>
2019-07-13 19:11:03 -0700

    Updates

---
+++ precache-manifest.e282492f9690689a52538866602b2792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc66a199dc4d416f5a8d50fd725b904",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "ff6a096989e6c38bdd93",
+    "url": "/Clicky-Game/static/css/main.2144db87.chunk.css"
+  },
+  {
+    "revision": "ed67e21e0fbb680b5f8f",
+    "url": "/Clicky-Game/static/js/2.abafc83a.chunk.js"
+  },
+  {
+    "revision": "ff6a096989e6c38bdd93",
+    "url": "/Clicky-Game/static/js/main.4e98b6cb.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed723976c767e5c081039c3feed35e19.js"
+  "/Clicky-Game/precache-manifest.e282492f9690689a52538866602b2792.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Clicky-Game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
